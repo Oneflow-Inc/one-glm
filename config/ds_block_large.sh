4c31a4ca88dfc0526092ecbe496e35dd02e5678e
fengwen <ccsuwen@gmail.com>
2023-01-13 02:37:43 +0000

    [DDP]

--- config/ds_block_large.sh
+++ config/ds_block_large.sh
@@ -27,6 +27,4 @@ gpt_options=" \
        --lr-decay-iters 160000 \
        --lr-decay-ratio 0.05 \
        --warmup .05 \
-       --checkpoint-activations \
-       --deepspeed-activation-checkpointing \
 "
